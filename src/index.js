9824d4cefc912000a45f3bbd6b1c320107a64fe2
kuus <kunderikuus@gmail.com>
2021-03-16 08:45:55 +0100

    export _ helper from reactIntl hook)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import { useIntl as reactUseIntl } from "react-intl";
+
 // export * from "react-intl";
-export { useIntl, FormattedMessage } from "react-intl";
+export { FormattedMessage } from "react-intl";
+export const useIntl = reactUseIntl;
+export const _ = () => useIntl().formatMessage;
+
 export {
   default as Link,
   navigate,
   changeLocale,
   getCurrentRoute,
 } from "./link";
+
 export { normaliseUrlPath, findRouteForPath } from "./utils";
+
 export { I18nConsumer, useI18n } from "./I18nContext";
