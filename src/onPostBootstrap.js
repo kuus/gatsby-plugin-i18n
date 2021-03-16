ca31269393d521cafb6e7cfd49b321efdd69d054
kuus <kunderikuus@gmail.com>
2021-03-16 11:04:38 +0100

    try to remove splaat redirect

--- src/onPostBootstrap.js
+++ src/onPostBootstrap.js
@@ -1,30 +1,30 @@
 // @ts-check
 
-const { getOptions } = require("./options");
-const { shouldCreateLocalisedPage } = require("./utils-plugin");
+// const { getOptions } = require("./options");
+// const { shouldCreateLocalisedPage } = require("./utils-plugin");
 
 const onPostBootstrap = ({ actions }, pluginOptions) => {
-  const { createRedirect } = actions;
-  const options = getOptions(pluginOptions);
+  // const { createRedirect } = actions;
+  // const options = getOptions(pluginOptions);
 
-  // create these redirects for netlify:
-  // @see https://answers.netlify.com/t/custom-localised-404-page-not-working/8842
-  options.locales.forEach((locale) => {
-    const visibleLocale = shouldCreateLocalisedPage(options, locale);
+  // // create these redirects for netlify:
+  // // @see https://answers.netlify.com/t/custom-localised-404-page-not-working/8842
+  // options.locales.forEach((locale) => {
+  //   const visibleLocale = shouldCreateLocalisedPage(options, locale);
 
-    if (visibleLocale) {
-      const redirect = {
-        fromPath: "/*",
-        toPath: `/${locale}/:splat`,
-        isPermanent: true,
-      };
-      if (locale !== options.defaultLocale) {
-        redirect.Language = locale;
-      }
+  //   if (visibleLocale) {
+  //     const redirect = {
+  //       fromPath: "/*",
+  //       toPath: `/${locale}/:splat`,
+  //       isPermanent: true,
+  //     };
+  //     if (locale !== options.defaultLocale) {
+  //       redirect.Language = locale;
+  //     }
 
-      createRedirect(redirect);
-    }
-  });
+  //     createRedirect(redirect);
+  //   }
+  // });
 };
 
 module.exports = onPostBootstrap;
