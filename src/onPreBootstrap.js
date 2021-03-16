233da0de0505910c0722f952ec2160d33a5954ba
kuus <kunderikuus@gmail.com>
2021-03-16 19:41:15 +0100

    bump, add onPreBootstrap redirects, remove redirect options, remove enforceLocalisedUrls, maybe there is just no use for it

--- src/onPreBootstrap.js
+++ src/onPreBootstrap.js
@@ -21,18 +21,38 @@ const onPreBootstrap = ({ actions }, pluginOptions) => {
   // / /en/ 301
   // / /it/ 301 Language=it
   // / /nl/ 301 Language=nl
+  //
+  // create these redirects for netlify:
+  // @see https://answers.netlify.com/t/custom-localised-404-page-not-working/8842
   options.locales.forEach((locale) => {
     const visibleLocale = shouldCreateLocalisedPage(options, locale);
 
     if (visibleLocale) {
-      const redirect = {
+      const redirectRoot = {
         fromPath: "/",
         toPath: `/${locale}/`,
         isPermanent: true,
         Language: locale,
       };
+      
+      createRedirect(redirectRoot);
+    }
 
-      createRedirect(redirect);
+    if (visibleLocale) {
+      const redirectAll = {
+        fromPath: "/*",
+        toPath: `/${locale}/:splat`,
+        isPermanent: true,
+      };
+      // only non-default locales get the Language option as the default does 
+      // not need it, request should by default follow the default localised
+      // urls, either if they have visible locale or not (according to the
+      // option `hideDefaultLocaleInUrl`)
+      if (locale !== options.defaultLocale) {
+        redirectAll.Language = locale;
+      }
+
+      createRedirect(redirectAll);
     }
   });
 };
