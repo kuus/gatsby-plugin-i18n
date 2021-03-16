32011e7cfd460a08411d84f3e3d2140267d7eba5
kuus <kunderikuus@gmail.com>
2021-03-16 11:09:27 +0100

    bump

--- src/onPostBootstrap.js
+++ src/onPostBootstrap.js
@@ -6,12 +6,10 @@
 const onPostBootstrap = ({ actions }, pluginOptions) => {
   // const { createRedirect } = actions;
   // const options = getOptions(pluginOptions);
-
   // // create these redirects for netlify:
   // // @see https://answers.netlify.com/t/custom-localised-404-page-not-working/8842
   // options.locales.forEach((locale) => {
   //   const visibleLocale = shouldCreateLocalisedPage(options, locale);
-
   //   if (visibleLocale) {
   //     const redirect = {
   //       fromPath: "/*",
@@ -21,7 +19,6 @@ const onPostBootstrap = ({ actions }, pluginOptions) => {
   //     if (locale !== options.defaultLocale) {
   //       redirect.Language = locale;
   //     }
-
   //     createRedirect(redirect);
   //   }
   // });
