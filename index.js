ca348e454bf37fae93eef827fd97c8604f7ed148
kuus <kunderikuus@gmail.com>
2021-06-15 13:11:24 +0200

    add re-rexport from react-intl

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ export { I18nSwitchLinks } from "./src/components/I18nSwitchLinks";
 export { I18nSwitchSelect } from "./src/components/I18nSwitchSelect";
 export { Link } from "./src/components/Link";
 export { navigate, useIntl, t } from "./src/helpers";
+export { FormattedDate, FormattedTime } from "react-intl";
